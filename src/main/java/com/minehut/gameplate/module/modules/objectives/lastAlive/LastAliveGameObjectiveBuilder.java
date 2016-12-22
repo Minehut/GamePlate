a04cadfb93befb78bc2fa0594bc2e1e7f1e6699c
Luke Chatton <chattonluke@gmail.com>
2016-12-21 21:57:21 -0700

    lastAlive game objective

---
+++ src/main/java/com/minehut/gameplate/module/modules/objectives/lastAlive/LastAliveGameObjectiveBuilder.java
@@ -0,0 +1,31 @@
+package com.minehut.gameplate.module.modules.objectives.lastAlive;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.minehut.gameplate.match.Match;
+import com.minehut.gameplate.module.*;
+import com.minehut.gameplate.module.modules.team.TeamModule;
+
+/**
+ * Created by luke on 12/21/16.
+ */
+@BuilderData(load = ModuleLoadTime.EARLIER)
+public class LastAliveGameObjectiveBuilder extends ModuleBuilder {
+
+    @Override
+    public ModuleCollection<? extends Module> load(Match match) {
+        if (match.getJson().has("objectives")) {
+            for (JsonElement e : match.getJson().getAsJsonArray("objectives")) {
+                JsonObject objectiveJson = e.getAsJsonObject();
+
+                if (objectiveJson.has("objective")) {
+                    if (objectiveJson.get("objective").getAsString().equalsIgnoreCase("last-alive")) {
+                        return new ModuleCollection<>(new LastAliveGameObjectiveModule());
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+}
---
+++ src/main/java/com/minehut/gameplate/module/modules/objectives/lastAlive/LastAliveGameObjectiveModule.java
@@ -0,0 +1,13 @@
+package com.minehut.gameplate.module.modules.objectives.lastAlive;
+
+import com.minehut.gameplate.module.GameObjectiveModule;
+
+/**
+ * Created by luke on 12/21/16.
+ */
+public class LastAliveGameObjectiveModule extends GameObjectiveModule {
+
+    public LastAliveGameObjectiveModule() {
+        super("last-alive", "Last Alive", false);
+    }
+}
