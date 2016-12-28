10b3a163f5be49e26c6c0dabdf2f7d577ff00dcb
Luke Chatton <chattonluke@gmail.com>
2016-12-27 18:34:10 -0800

    Use xml in lives module

--- src/main/java/com/minehut/gameplate/module/modules/lives/LivesModuleBuilder.java
+++ src/main/java/com/minehut/gameplate/module/modules/lives/LivesModuleBuilder.java
@@ -8,6 +8,8 @@ import com.minehut.gameplate.module.ModuleCollection;
 import com.minehut.gameplate.module.modules.observers.ObserverModule;
 import com.minehut.gameplate.module.modules.team.TeamModule;
 import com.minehut.gameplate.module.modules.teamManager.TeamManager;
+import com.minehut.gameplate.util.Numbers;
+import org.jdom2.Element;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -21,13 +23,12 @@ public class LivesModuleBuilder extends ModuleBuilder {
     public ModuleCollection<? extends Module> load(Match match) {
         HashMap<TeamModule, Integer> lives = new HashMap<>();
 
-        if (match.getJson().has("lives")) {
-            JsonObject livesJson = match.getJson().getAsJsonObject("lives");
-            for (TeamModule teamModule : TeamManager.getTeamModules()) {
-                if(teamModule.isObserver()) continue;
-
-                if (livesJson.has(teamModule.getId())) {
-                    lives.put(teamModule, livesJson.get(teamModule.getId()).getAsInt());
+        for (Element livesElement : match.getDocument().getRootElement().getChildren("respawns")) {
+            for (Element element : livesElement.getChildren()) {
+                if (element.getName().toLowerCase().equals("respawn")) {
+                    TeamModule teamModule = TeamManager.getTeamById(element.getAttributeValue("team"));
+                    int amount = Numbers.parseInt(element.getAttributeValue("lives"));
+                    lives.put(teamModule, amount);
                 }
             }
         }
