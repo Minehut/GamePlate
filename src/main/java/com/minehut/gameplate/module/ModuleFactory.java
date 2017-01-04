9c886591381d26eabc163be6d23c4edab55633ab
Trophonix <502badgamer@gmail.com>
2017-01-04 17:33:23 -0600

    ItemDropModule work

--- src/main/java/com/minehut/gameplate/module/ModuleFactory.java
+++ src/main/java/com/minehut/gameplate/module/ModuleFactory.java
@@ -11,6 +11,7 @@ import com.minehut.gameplate.module.modules.filter.FilterModuleBuilder;
 import com.minehut.gameplate.module.modules.friendlyFire.FriendlyFireModuleBuilder;
 import com.minehut.gameplate.module.modules.gameComplete.GameCompleteModuleBuilder;
 import com.minehut.gameplate.module.modules.inventoryView.InventoryViewModuleBuilder;
+import com.minehut.gameplate.module.modules.itemDrop.ItemDropModuleBuilder;
 import com.minehut.gameplate.module.modules.kit.KitModuleBuilder;
 import com.minehut.gameplate.module.modules.lives.LivesModuleBuilder;
 import com.minehut.gameplate.module.modules.matchAlerts.MatchAlertsModuleBuilder;
@@ -81,7 +82,8 @@ public class ModuleFactory {
                 TabHeaderBuilder.class,
                 DestroyableObjectiveBuilder.class,
                 PortalModuleBuilder.class,
-                AxeDamageModuleBuilder.class
+                AxeDamageModuleBuilder.class,
+                ItemDropModuleBuilder.class
         ));
     }
 
--- src/main/java/com/minehut/gameplate/module/modules/itemDrop/ItemDropModule.java
+++ src/main/java/com/minehut/gameplate/module/modules/itemDrop/ItemDropModule.java
@@ -1,12 +1,10 @@
 package com.minehut.gameplate.module.modules.itemDrop;
 
-import com.minehut.gameplate.module.Module;
 import com.minehut.gameplate.module.TaskedModule;
 import org.bukkit.Location;
 import org.bukkit.entity.Item;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.List;
+import org.bukkit.util.Vector;
 
 /**
  * Created by Lucas on 1/4/2017.
@@ -19,12 +17,12 @@ public abstract class ItemDropModule extends TaskedModule {
     public ItemDropModule(Location location, ItemStack item) {
         this.location = location;
         this.item = item;
-        respawn();
     }
 
     public Item respawn() {
-        Item itemSpawn = location.getWorld().dropItemNaturally(location, item);
-        itemSpawn.setPickupDelay(20 * 3);
+        Item itemSpawn = location.getWorld().dropItem(location, item);
+        itemSpawn.setVelocity(new Vector(0, 0, 0));
+        itemSpawn.setPickupDelay(20 * 2);
         return itemSpawn;
     }
 
